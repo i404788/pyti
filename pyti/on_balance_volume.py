36e31f7e6691c2d1caf3b67824c7d4a586c0bc76
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2018-09-13 16:15:48 +0200

    Added Percentage variation of OnBalanceVolume. This indicator can be used to compare different series that have different volumes.

--- pyti/on_balance_volume.py
+++ pyti/on_balance_volume.py
@@ -28,3 +28,34 @@ def on_balance_volume(close_data, volume):
         elif close_data[idx] == close_data[idx-1]:
             obv[idx] = obv[idx-1]
     return obv
+
+
+def on_balance_volume_percentage(close_data, volume):
+    """
+    On Balance Volume Percentage. Count an accumulative percentage
+    of volume oscillations, instead of its absolute value.
+
+    Formula:
+    start = 1
+    if CLOSEt > CLOSEt-1
+        obv = obvt-1 + (volumet - volumet-1) / volumet-1
+    elif CLOSEt < CLOSEt-1
+        obv = obvt-1 - (volumet - volumet-1) / volumet-1
+    elif CLOSEt == CLOSTt-1
+        obv = obvt-1
+    """
+    catch_errors.check_for_input_len_diff(close_data, volume)
+    obv = np.zeros(len(volume))
+    obv[0] = 1
+    for idx in range(1, len(obv)):
+        if volume[idx-1] == 0:
+            volume_var = 100 * (volume[idx] - volume[idx-1])
+        else:
+            volume_var = 100 * (volume[idx] - volume[idx-1]) / volume[idx-1]
+        if close_data[idx] > close_data[idx-1]:
+            obv[idx] = obv[idx-1] + volume_var
+        elif close_data[idx] < close_data[idx-1]:
+            obv[idx] = obv[idx-1] - volume_var
+        elif close_data[idx] == close_data[idx-1]:
+            obv[idx] = obv[idx-1]
+    return obv
